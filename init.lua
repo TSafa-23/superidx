48c9c84a62b648652feee9663744610a22bd36e0
Apelta <54854228+TSafa-23@users.noreply.github.com>
2021-06-19 21:36:27 -0500

    RLE Compression/loading
    null file loading

--- init.lua
+++ init.lua
@@ -1,15 +1,44 @@
-hyload = {}
+superi = {}
 
-hyload.saved = {}
+superi.saved = {}
 
-function hyload.iterate_to(number, constant)
+function superi.iterate_to(number, constant)
 	if number < constant then return number + 1
 	else return number - 1 end
 end
 
 --------------------------------------------------------------------------------------------------------------
+
+function superi.rle(nodes)
+	local ti = 1
+	local tstr = ""
+	local kvp = {}
+
+	local nodes_rle = {} -- new table
+
+	for i = 1, #nodes do
+
+		if nodes[i] ~= nodes[i+1] then
+			tstr = "{" ..nodes[i] .."," ..ti .."}"
+			if #tstr > ti then
+				for e = 1, ti do
+					table.insert(nodes_rle, nodes[i])
+				end
+			else
+				table.insert(nodes_rle, {nodes[i], ti})
+			end
+			ti = 1
+		else
+			ti = ti + 1
+		end
+
+	end
+	return nodes_rle
+end
+
+--------------------------------------------------------------------------------------------------------------
 -- pos1 and pos2 *must* be tables of x, y and z coordinates.
-function hyload.save(pos1, pos2, name)
+function superi.save(pos1, pos2, name)
 
 	local nodenames = {}
 	local nodes = {}
@@ -40,54 +69,77 @@ function hyload.save(pos1, pos2, name)
 					table.insert(nodenames, tempnode.name)
 					table.insert(nodes, #nodenames)
 				end
-				x = hyload.iterate_to(x, l)
+				x = superi.iterate_to(x, l)
 			end
 			x = 0
-			z = hyload.iterate_to(z, w)
+			z = superi.iterate_to(z, w)
 		end
 		z = 0
-		y = hyload.iterate_to(y, h)
+		y = superi.iterate_to(y, h)
 	end
-	hyload.saved[name] = {l = l, w = w, h = h, nodenames = nodenames, nodes = nodes}
-	print(minetest.serialize(hyload.saved[name]):gsub(" ", ""))
+
+	superi.saved[name] = {l = l, w = w, h = h, nodenames = nodenames, nodes = superi.rle(nodes)}
+
+	minetest.mkdir(minetest.get_worldpath() .."/schems")
+	local file = io.open(minetest.get_worldpath() .."/schems/" ..name ..".sdx", "w+")
+	file:write(minetest.serialize(superi.saved[name]):gsub(" ", ""))
+	file:close()
+
 end
 
 -------------------------------------------------------------------------------------------------------------
 
-function hyload.load(pos, name)
+function superi.load(pos, name)
+
+	local i = 1
+	local ti = 1
 	local x = 0
 	local y = 0
 	local z = 0
-	local i = 1
-
-	local pos2 = {x = pos.x + hyload.saved[name].l, y = hyload.saved[name].h, z = hyload.saved[name].w}
+	local data = superi.saved[name] or minetest.deserialize(io.open(minetest.get_worldpath() .."/schems/" ..name ..".sdx", "r"))
+	local pos2 = {x = pos.x + data.l, y = pos.y + data.h, z = pos.z + data.w}
 
 	minetest.emerge_area(pos, pos2)
 
-	while y ~= hyload.saved[name].h do
-		while z ~= hyload.saved[name].w do
-			while x ~= hyload.saved[name].l do
-				minetest.set_node({x = pos.x + x, y = pos.y + y, z = pos.z + z}, {name = hyload.saved[name].nodenames[hyload.saved[name].nodes[i]]})
-				i = i + 1
-				x = hyload.iterate_to(x, hyload.saved[name].l)
+	while y ~= data.h do
+		while z ~= data.w do
+			while x ~= data.l do
+
+				if data.nodenames[data.nodes[i]] then
+					minetest.set_node({x = x + pos.x, y = y + pos.y, z = z + pos.z}, {name = data.nodenames[data.nodes[i]]})
+					i = i + 1
+				else
+
+					if ti < data.nodes[i][2] then
+						ti = ti + 1
+						minetest.set_node({x = x + pos.x, y = y + pos.y, z = z + pos.z}, {name = data.nodenames[data.nodes[i][1]]})
+					else
+						minetest.set_node({x = x + pos.x, y = y + pos.y, z = z + pos.z}, {name = data.nodenames[data.nodes[i][1]]})
+						i = i + 1
+						ti = 1
+					end
+
+				end
+
+				x = superi.iterate_to(x, data.l)
 			end
 			x = 0
-			z = hyload.iterate_to(z, hyload.saved[name].w)
+			z = superi.iterate_to(z, data.w)
 		end
 		z = 0
-		y = hyload.iterate_to(y, hyload.saved[name].h)
+		y = superi.iterate_to(y, data.h)
 	end
 end
 
 -------------------------------------------------------------------------------------------------------------
 
 local uwu = {x = 0, y = 0, z = 0}
-local owo = {x = 200, y = 200, z = 200}
+local owo = {x = -200, y = -200, z = -200}
 
-minetest.register_chatcommand("uwu", {
-	func = function()
-		hyload.save(uwu, owo, "uwu")
-		minetest.chat_send_all("Finished!")
+minetest.register_chatcommand("save", {
+	func = function(name, param)
+		superi.save(uwu, owo, param)
+		minetest.chat_send_all("Saved as " ..param ..".sdx!")
 	end
 })
 
@@ -99,14 +151,14 @@ minetest.register_chatcommand("emerge", {
 })
 
 
-minetest.register_chatcommand("owo", {
-	func = function()
-		hyload.load(uwu, "uwu")
-		minetest.chat_send_all("Finished!")
+minetest.register_chatcommand("load", {
+	func = function(name, param)
+		superi.load(uwu, param)
+		minetest.chat_send_all("Loaded " ..param ..".sdx!")
 	end
 })
 
-minetest.register_chatcommand("/1", {
+minetest.register_chatcommand("1", {
 	func = function(name)
 		local tpos = minetest.get_player_by_name(name):get_pos()
 		uwu = {x = math.floor(tpos.x), y = math.floor(tpos.y), z = math.floor(tpos.z)}
@@ -114,7 +166,7 @@ minetest.register_chatcommand("/1", {
 	end
 })
 
-minetest.register_chatcommand("/2", {
+minetest.register_chatcommand("2", {
 	func = function(name)
 		local tpos = minetest.get_player_by_name(name):get_pos()
 		owo = {x = math.floor(tpos.x), y = math.floor(tpos.y), z = math.floor(tpos.z)}
